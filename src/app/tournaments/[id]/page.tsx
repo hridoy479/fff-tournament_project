691e50603b855a979e73525b956fbf8e5cefb4e4
hridoymolla479 <hridoymolla479@gmail.com>
2025-07-27 19:08:22 -0700

    add id page

---
+++ src/app/tournaments/[id]/page.tsx
@@ -0,0 +1,13 @@
+import { Main } from '@/components/Main'
+import TournamentDetails from '@/components/TournamentDetails'
+import React from 'react'
+
+function page() {
+  return (
+    <Main>
+        <TournamentDetails />
+    </Main>
+  )
+}
+
+export default page
