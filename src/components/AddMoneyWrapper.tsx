f0d73b5602e7537a10b145e09c684a5b07879f17
hridoymolla479 <hridoymolla479@gmail.com>
2025-07-27 19:11:17 -0700

    delete addmoney wrapper

--- src/components/AddMoneyWrapper.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const AddMoney = dynamic(() => import('./AddMoney'), { ssr: false });
-
-export default function AddMoneyWrapper() {
-  return <AddMoney />;
-}
